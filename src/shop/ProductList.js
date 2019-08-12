59a33914578a5e5b5b17b4390a7179162d123e0a
Jonathan Sees <jonathan.m.sees@gmail.com>
2019-08-12 14:04:46 -0700

    Shopping Cart

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -5,16 +5,21 @@ export class ProductList extends Component {
         if (this.props.products == null || this.props.products.length === 0) {
             return <h5 className="p-2">No Products</h5>
         }
-        return this.props.products.map (p =>
-            <div className="card m-1 p-1 bg-light" key={ p.id }>
+        return this.props.products.map (product =>
+            <div className="card m-1 p-1 bg-light" key={ product.id }>
                 <h4>
-                    { p.name }
+                    { product.name }
                     <span className="badge badge-pill badge-primary float-right">
-                        ${ p.price.toFixed(2) }
+                        ${ product.price.toFixed(2) }
                     </span>
                 </h4>
                 <div className="card-text bg-white p-1">
-                    { p.description }
+                    { product.description }
+                    <button 
+                        className="btn btn-success btn-sm float-right"
+                        onClick={ () => this.props.addToCart(product) }>
+                        Add to Cart
+                    </button>
                 </div>
             </div>
         )
